5e5ab4576c356838ba49561b5b76a7333b60c12e
unknown <kamrulhasananik019@gmail.com>
2023-05-04 23:19:27 +0600

    PrivateRoute

---
+++ src/Route/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../Provider/AuthProvider';
+import { Navigate, useLocation } from 'react-router-dom';
+import { Spinner } from 'flowbite-react';
+
+const PrivateRoute = ({ children }) => {
+    const { user, loader } = useContext(AuthContext);
+    const location = useLocation()
+
+    if (loader) {
+        return <Spinner aria-label="Default status example" />
+    }
+    if (user) {
+        return children
+    }
+    return (
+        <Navigate state={{ from: location }} to='/login'></Navigate>
+    );
+};
+
+export default PrivateRoute;
