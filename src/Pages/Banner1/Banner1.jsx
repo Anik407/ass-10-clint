32978494af3dca72baba6bdf814eacb17bbd080a
unknown <kamrulhasananik019@gmail.com>
2023-05-04 18:13:05 +0600

    Banner is Ok

--- src/Pages/Banner1/Banner1.jsx
+++ src/Pages/Banner1/Banner1.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Banner1 = () => {
     return (
-        <div>
-            this is banner
+        <div className='my-10' >
+            <div className=' grid grid-cols-1 md:grid-cols-2'>
+                <div className=' p-5 md:p-36'>
+                    <h1 className='my-2 text-4xl md:text-6xl font-bold'>
+                   <span className='text-orange-400'> Cook's</span> <span className='text-lime-300'>Corner</span>
+                    </h1>
+                    <h2 className='text-2xl font-semibold'>Discover the Ultimate Destination for Foodies:    <span className='text-orange-400'> Cook's</span> <span className='text-lime-300'>Corner</span> - Your Go-To Site for Mouth-Watering Recipes, Culinary Tips, and Delicious Inspiration!</h2>
+                </div>
+                <div>
+                    <img src="https://img.freepik.com/free-photo/impressed-young-handsome-cook-chef-uniform-standing-white-wall-pointing-with-hand-it-isolated-orange-wall-with-copy-space_141793-101494.jpg?size=626&ext=jpg" alt="" />                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Banner from './Banner/Banner';
 import { useLoaderData } from 'react-router-dom';
 import AllChef from './AllChef/AllChef';
+import Banner1 from '../Banner1/Banner1';
 
 const Home = () => {
     const chef = useLoaderData();
    
     return (
         <div>
-         
+         <Banner1></Banner1>
             <AllChef chef={chef}></AllChef>
             <Banner></Banner>
        
