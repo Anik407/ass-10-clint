d2e23b3b2b7544ef2023ff6f9f531dac44d78e33
unknown <kamrulhasananik019@gmail.com>
2023-06-22 22:51:57 +0600

    first commit

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
     const [error, setError] = useState('')
     const navigate = useNavigate()
     const location = useLocation()
-   
+
     const from = location.state?.from?.pathname || '/';
 
     const handleLogin = (event) => {
